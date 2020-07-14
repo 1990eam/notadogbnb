4011e03a45de950ad9ab991b320fb113672284f8
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-07-14 20:50:00 -0300

    all seeds are functional plus ActiveStorage association so notdogs can have images

--- app/models/notdog.rb
+++ app/models/notdog.rb
@@ -1,5 +1,6 @@
 class Notdog < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: 'user_id'
   has_many :bookings
+  has_one_attached :photo
   validates :owner, :cost_per_day, :description, :name, presence: true
 end
