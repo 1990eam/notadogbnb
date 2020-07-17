e0ad83e6f4085880324145621098ddda9ae11608
Elian Magni <magni.elian@gmail.com>
2020-07-16 21:04:54 -0300

    changed validations from time to start_date and end_date for new bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :notdog
   belongs_to :user
   has_one :owner, through: :notdog
-  validates :time, :notdog, :user, presence: true
+  validates :notdog, :start_date, :end_date, :user, presence: true
 end
