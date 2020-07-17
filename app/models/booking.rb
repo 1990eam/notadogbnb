901461fbc1cf3c114b4c498a51cf4f5cd0dde2ab
Tomas Hergott <tomashergott@gmail.com>
2020-07-16 21:55:06 -0300

    Nueva validacion en la clase Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_one :owner, through: :notdog
   validates :notdog, :start_date, :end_date, :user, presence: true
+  validate :start_date_lower_than_end_date
+
+  def start_date_lower_than_end_date
+    if start_date > end_date
+      errors.add(:start_date, "No puede ser mayor que la fecha final")
+    end
+  end
 end
