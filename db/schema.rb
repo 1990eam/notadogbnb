1f9e24d79aefece11046e002db0201c2e9585880
Amy_Camacho <amyamyc@hotmail.com>
2020-07-18 15:51:08 -0300

    Datos de schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_144556) do
+ActiveRecord::Schema.define(version: 2020_07_18_160208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,17 @@ ActiveRecord::Schema.define(version: 2020_07_18_144556) do
     t.index ["user_id"], name: "index_notdogs_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "user_review"
+    t.integer "user_score"
+    t.string "owner_review"
+    t.integer "owner_score"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +94,5 @@ ActiveRecord::Schema.define(version: 2020_07_18_144556) do
   add_foreign_key "bookings", "notdogs"
   add_foreign_key "bookings", "users"
   add_foreign_key "notdogs", "users"
+  add_foreign_key "reviews", "users"
 end
