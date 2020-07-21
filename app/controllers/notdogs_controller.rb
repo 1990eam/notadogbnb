8dae486351a1eabe6f22d8b03bc1f2e9aa3418f3
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-07-20 21:14:05 -0300

    added scope validations for users... which don't work, but they are there

--- app/controllers/notdogs_controller.rb
+++ app/controllers/notdogs_controller.rb
@@ -2,7 +2,7 @@ class NotdogsController < ApplicationController
   before_action :set_notdog, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    @notdogs = policy_scope(Notdog.where(owner: !current_user))
+    @notdogs = policy_scope(Notdog).order(created_at: :desc)
     @notdogs = Notdog.geocoded
 
     @markers = @notdogs.map do |notdog|
