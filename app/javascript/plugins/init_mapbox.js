56ea249ecb5c942aed9b47cd42424d2d1fade2fa
Tomas Hergott <tomashergott@gmail.com>
2020-07-22 17:03:49 -0300

    Mapas individuales por cada notdog agregados

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,6 +8,7 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2500 });
 };
 
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -15,13 +16,13 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/tomashergott/ckcwli9fx00bx1jpbqmt4dx17'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-      // Esto rompe el marker creo, no puedo hacer que muestre la imagen
+
       const element = document.createElement('div');
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
@@ -42,6 +43,34 @@ const initMapbox = () => {
   };
 };
 
+const initMapboxForCards = () => {
+  const mapElement = document.getElementById('map-card');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map-card',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+
+      new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    });
+    fitMapToMarkers(map, markers)
+  };
+};
+
 
 export { initMapbox };
-
+export { initMapboxForCards };
