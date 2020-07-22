1f9f8fe2c9731351235a07f7327a99ac62a994dd
Elian Magni <magni.elian@gmail.com>
2020-07-21 21:08:41 -0300

    added new message form partial. added new message view. started working on message_policy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
 
   def new
-
+    @message = Message.new
+    @notdog = Notdog.find(params[:notdog_id])
+    authorize @message
+    authorize @notdog
   end
 
   def create
