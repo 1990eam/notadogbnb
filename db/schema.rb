4ebfa8ed979432e694a915723ead7850a01868d1
Elian Magni <magni.elian@gmail.com>
2020-07-22 17:21:59 -0300

    added Answer column to Messages to enable replies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_233344) do
+ActiveRecord::Schema.define(version: 2020_07_22_202108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_07_21_233344) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "answer"
     t.index ["notdog_id"], name: "index_messages_on_notdog_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
