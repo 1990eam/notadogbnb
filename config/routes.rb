12da0efe5c87cd7e861b253b35d0f90b766bf4d9
Elian Magni <magni.elian@gmail.com>
2020-07-23 22:01:51 -0300

    added route for user/bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :bookings, only: [:show]
+  end
   resources :notdogs do
     resources :bookings, only: [:new, :create]
     resources :messages, only: [:new, :create, :edit, :update, :destroy]
