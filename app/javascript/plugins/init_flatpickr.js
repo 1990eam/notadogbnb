81701dbf27abb649ff2c436aedc0c1a57b0009dd
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-07-25 12:45:35 -0300

    more frontend

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -2,20 +2,27 @@ import flatpickr from "flatpickr";
 
 let array = [];
 
-var datesHash = document.querySelectorAll("#invalid-dates").forEach(element =>
+document.querySelectorAll("#invalid-dates").forEach(element =>
   array[array.length] = ({
     from: element.innerText.split(" - ")[0],
     to: element.innerText.split(" - ")[1]
   })
   );
 
-console.log(array);
-
-flatpickr(".datepicker", {
-    mode: "range",
-    dateFormat: "Y-m-d",
-    disable:
-    array
-});
+if (document.querySelector("#invalid-dates")) {
+  if (array.length > 0) {
+    flatpickr(".datepicker", {
+        mode: "range",
+        dateFormat: "Y-m-d",
+        disable:
+        array
+    });
+  } else {
+    flatpickr(".datepicker", {
+        mode: "range",
+        dateFormat: "Y-m-d"
+    });
+  }
+}
 
 
