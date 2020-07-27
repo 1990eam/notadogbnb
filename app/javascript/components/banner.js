d970a8e686441bb6c899ac6094891c8344b40143
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-07-27 19:56:11 -0300

    more flatpickr testing

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,17 +1,19 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Dream about petting sharks? ^500", "Dream about a Notdog! ^2000"],
-    typeSpeed: 80,
-    backSpeed: 50,
-    showCursor: true,
-    smartBackspace: true,
-    loop: true,
-    showCursor: true,
-    cursorChar: '|',
-    autoInsertCss: true,
+  if (document.querySelector('#banner-typed-text')) {
+    new Typed('#banner-typed-text', {
+      strings: ["Dream about petting sharks? ^500", "Dream about a Notdog! ^2000"],
+      typeSpeed: 80,
+      backSpeed: 50,
+      showCursor: true,
+      smartBackspace: true,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+      autoInsertCss: true,
   });
+  }
 }
 
 
